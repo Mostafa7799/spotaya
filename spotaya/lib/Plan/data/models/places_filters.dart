08f9f35cccbec3ea2268622d4dbb692bfe71e560
Mohamed Mohamedy <mohamedmohamedy146@gmail.com>
2023-05-08 00:40:34 +0300

    Implement data sources and models

--- spotaya/lib/Plan/data/models/places_filters.dart
+++ spotaya/lib/Plan/data/models/places_filters.dart
@@ -1,10 +1,22 @@
 import 'package:equatable/equatable.dart';
 
 class PlacesFilters extends Equatable {
+  final String lat, log, fromTime, toTime, types;
 
+  const PlacesFilters({
+    required this.lat,
+    required this.log,
+    required this.fromTime,
+    required this.toTime,
+    required this.types,
+  });
 
   @override
   List<Object?> get props => [
-        
+        lat,
+        log,
+        fromTime,
+        toTime,
+        types,
       ];
 }
--- spotaya/lib/Plan/data/models/places_model.dart
+++ spotaya/lib/Plan/data/models/places_model.dart
@@ -1,9 +1,35 @@
 import 'package:equatable/equatable.dart';
 
 class Place extends Equatable {
-  final String title, imageUrl;
-  const Place({required this.title, required this.imageUrl});
+  final String name, imageUrl, location, activityDuration;
+  final List<String> types;
+  const Place({
+    required this.location,
+    required this.types,
+    required this.name,
+    required this.imageUrl,
+    required this.activityDuration,
+  });
+
+  factory Place.fromJson(Map<String, dynamic> json) {
+    List<String> types = [];
+    if (json['types'] != null) {
+      types = List<String>.from(json['types'].map((type) => type.toString()));
+    }
+    return Place(
+      location: json['location'] ?? 'none',
+      types: types,
+      name: json['name'] ?? 'none',
+      imageUrl: json['image'] ?? 'none',
+      activityDuration: json['activity_duration'] ?? 'none',
+    );
+  }
 
   @override
-  List<Object> get props => [title, imageUrl];
+  List<Object> get props => [
+        location,
+        types,
+        name,
+        imageUrl,
+      ];
 }
